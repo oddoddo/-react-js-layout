50f11f73a7004a96b42402127254df0ff76d9ee6
odd:odd <eehd80yul@gmail.com>
2024-02-06 11:14:22 +0900

    first commit

---
+++ src/components/layout/Gnb.jsx
@@ -0,0 +1,23 @@
+import { UnorderedList, ListItem } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+
+const Gnb = () => {
+    return (
+        <UnorderedList display={{ sm: 'none', lg: 'flex' }} gap={'40px'} fontSize={'16px'} color={'white'}>
+            <ListItem>
+                <Link to="/marketplace">NFT Marketplace</Link>
+            </ListItem>
+            <ListItem>
+                <Link to="/datatables">Data Tables</Link>
+            </ListItem>
+            <ListItem>
+                <Link to="/profile">Profile</Link>
+            </ListItem>
+            <ListItem>
+                <Link to="/signin">Sign In</Link>
+            </ListItem>
+        </UnorderedList>
+    )
+}
+
+export default Gnb
